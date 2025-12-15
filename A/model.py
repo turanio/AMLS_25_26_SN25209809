1c708184fb9bb4ee989fe63d98bdb194e289a9cf
Turan Ahmadli <turan.ahmadli@outlook.com>
2025-12-15 07:02:59 +0100

    feat: Implement SVM build_model function

---
+++ A/model.py
@@ -0,0 +1,25 @@
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+
+def build_model(config):
+    """build model function."""
+    steps = []
+
+    if config.standardize:
+        steps.append(("scaler", StandardScaler()))
+
+    steps.append(("pca", PCA(
+        n_components=config.pca_n_components,
+        random_state=config.seed
+    )))
+
+    steps.append(("svm", SVC(
+        kernel="linear",
+        C=config.svm_c,
+        random_state=config.seed
+    )))
+
+    return Pipeline(steps)
