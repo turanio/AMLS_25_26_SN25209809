44e544262dd297b3f5e3f9a522296923a33e6c62
Turan Ahmadli <turan.ahmadli@outlook.com>
2025-12-18 11:11:27 +0100

    feat: implement model config dataclass object

---
+++ A/config.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ModelConfig:
+    seed: int = 42
+
+    use_augmentation: bool = True
+    standardize: bool = True
+    pca_n_components: int = 64
+    svm_c: float = 1.0
