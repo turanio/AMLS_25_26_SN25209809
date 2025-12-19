5adc63e491113d2073ff8ecbfc21ac62f56ef3e1
Turan Ahmadli <turan.ahmadli@outlook.com>
2025-12-19 11:28:00 +0100

    feat: Update main.py to test model A

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+from data.breastmnist import BreastMNISTLoader
+from A.config import ModelConfig as SVMModelConfig
+from A.train import ModelATrainer
 
 
-def run_model_a():
-    pass
+def main():
+    loader = BreastMNISTLoader(
+        root_dir="datasets",
+        val_split=0.1,
+        test_split=0.2,
+        normalize=True,
+        seed=42,
+    )
+    data = loader.load()
 
-def run_model_b():
-    pass
+    config = SVMModelConfig(
+        seed=42,
+        pca_n_components=64,
+        svm_c=1.0,
+        use_augmentation=True
+    )
 
+    trainer = ModelATrainer(config)
+    results = trainer.run(data)
 
+    print("Model A results:", results)
 
-def orchestration():
-    pass
 
 if __name__ == "__main__":
-    orchestration()
+    main()
